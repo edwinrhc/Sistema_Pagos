83a4fd3377cae3bfe8f64a26bdf40a15dddafdc6
Edwin HC <edwinrhc@gmail.com>
2024-12-31 21:43:47 -0500

    Configuración inicial del proyecto Spring boot

---
+++ src/main/java/com/colegio/sam/sistemaspagos/SistemasPagosApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.sam.sistemaspagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasPagosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemasPagosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colegio/sam/sistemaspagos/SistemasPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio.sam.sistemaspagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasPagosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
